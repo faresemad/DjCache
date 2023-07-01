27e3ebe89cfc34553ee8894f5520489dee69065b
Fares Emad <faresemadx@gmail.com>
2023-07-02 00:14:48 +0300

    feat: add per-url cache

--- cacheapp/urls.py
+++ cacheapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from . import views
 
 app_name = "cacheapp"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", cache_page(60 * 16)(views.index), name="index"),
 ]
