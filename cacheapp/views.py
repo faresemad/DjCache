21b017686c5a55279880e25a8ddef1473392be2c
Fares Emad <faresemadx@gmail.com>
2023-07-02 00:11:38 +0300

    feat: add per-view cache

--- cacheapp/views.py
+++ cacheapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 
+
 # Create your views here.
-@cache_page(60 * 15) # 15 minutes
+@cache_page(60 * 15)  # 15 minutes
 def index(request):
     return render(request, "cacheapp/index.html", {})
